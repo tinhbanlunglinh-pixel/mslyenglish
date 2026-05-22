40f3ef62a7e29aa6f7cbaf281e2027552ff1c51c
tinhbanlunglinh-pixel <tinhbanlunglinh@gmail.com>
2026-05-22 18:19:34 +0700

    Add files via upload

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const BrandLogo = ({ className = "w-10 h-10" }: { className?: string }) => (
   <img 
-    src="https://i.postimg.cc/9XPytgYL/597de510-630c-466d-a059-48e352a46764.png" 
+    src="https://i.postimg.cc/KjfCRQ6Y/e15cb4f9-b2de-4779-8838-aafe20bc074f.png" 
     alt="Ms Lý English Logo" 
     className={`${className} object-contain`} 
   />
